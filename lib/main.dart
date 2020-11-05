8b29ce750fdfd1402949077147367c197b595ecb
Mehul-Bisht <mehulbishtanna@gmail.com>
2020-11-05 19:20:58 +0530

    Chapter-4

--- lib/main.dart
+++ lib/main.dart
@@ -25,39 +25,44 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        height: 300,
-        color: Colors.teal,
-        child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.red,
-                alignment: Alignment.center,
-              ),
-
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.yellow,
-                alignment: Alignment.center,
-              ),
-
-              Container(
-                padding: const EdgeInsets.all(8),
-                width: 100,
-                height: 100,
-                color: Colors.green,
-                alignment: Alignment.center,
-              )
-
-            ],
-          ),
-      )
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.teal,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Mehul Bisht"),
+              accountEmail: Text("mehulbishtanna@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1585737655161-76ddbd569e5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
+              )),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Mehul Bisht"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              onTap: () {} ,
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("mehulbishtanna@gmail.com"),
+              trailing: Icon(Icons.edit),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
+      ),
     );
   }
 }
