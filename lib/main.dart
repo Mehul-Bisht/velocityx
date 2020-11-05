875b102268cda9aa3f04bece29fa0d91da2560b3
Mehul-Bisht <mehulbishtanna@gmail.com>
2020-11-06 00:51:18 +0530

    Chapter-5

--- lib/main.dart
+++ lib/main.dart
@@ -18,37 +18,76 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var myText = "Change My Name";
+  TextEditingController _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
       body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          color: Colors.teal,
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Card(
+            child: Column(
+              children: [
+                Image.asset(
+                  "assets/jess.png",
+                  width: 150,
+                  height: 250,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                     controller: _nameController,
+                    decoration: InputDecoration(
+                        hintText: "Enter some text",
+                        labelText: "Name",
+                        border: OutlineInputBorder()),
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
-      ),
+      )),
       drawer: Drawer(
         child: ListView(
           padding: const EdgeInsets.all(0),
           children: [
             UserAccountsDrawerHeader(
-              accountName: Text("Mehul Bisht"),
-              accountEmail: Text("mehulbishtanna@gmail.com"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: NetworkImage(
-                    "https://images.unsplash.com/photo-1585737655161-76ddbd569e5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
-              )),
+                accountName: Text("Mehul Bisht"),
+                accountEmail: Text("mehulbishtanna@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1585737655161-76ddbd569e5a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"),
+                )),
             ListTile(
               leading: Icon(Icons.person),
               title: Text("Mehul Bisht"),
               subtitle: Text("Developer"),
               trailing: Icon(Icons.edit),
-              onTap: () {} ,
+              onTap: () {},
             ),
             ListTile(
               leading: Icon(Icons.email),
@@ -60,8 +99,11 @@ class MyHomePage extends StatelessWidget {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit),
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
       ),
     );
   }
